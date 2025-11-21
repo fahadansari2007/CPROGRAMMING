280c38948d0ca486d629d8a53579bea6b25dc3c0
Fahad Ansari <fahadansari2007@eng.rizvi.edu.in>
2025-11-21 14:10:09 +0530

    Create EX2A.C

---
+++ EX2A.C
@@ -0,0 +1,22 @@
+// Name: FAHAD ANSARI
+// UIN: 251C0007
+// Class: Civil
+// Div: B
+
+#include <stdio.h>
+
+void main()
+{
+    int a;
+    printf("Enter any number: ");
+    scanf("%d", &a);
+
+    if (a % 2 == 0)
+    {
+        printf("The number is even %d", a);
+    }
+    else
+    {
+        printf("The number is odd %d", a);
+    }
+}
