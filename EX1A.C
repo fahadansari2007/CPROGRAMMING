908490613c3b211bc9ef37b8fb3faa74b52485dc
Fahad Ansari <fahadansari2007@eng.rizvi.edu.in>
2025-11-21 14:07:47 +0530

    Create EX1A.C

---
+++ EX1A.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Fahad\n");
+    printf("Age: eighteen\n");
+    printf("Class: Civil\n");
+    printf("Division: B\n");
+    printf("UIN: 251C007\n");
+    return 0;
+}
