7b5f998e4b5f26f59937d3e77744b120964cfacd
Fahad Ansari <fahadansari2007@eng.rizvi.edu.in>
2025-11-21 14:19:22 +0530

    Create EX8A.C

---
+++ EX8A.C
@@ -0,0 +1,26 @@
+/* NAME: FAHAD ANSARI
+UIN :- 251C007
+CLASS:- CIVIL
+ROLL NO:- 7
+DIV:- B */
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, 100, stdin); 
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\n') {   
+            str[i] = '\0';      
+            break;              
+        }
+        length++;
+    }
+
+    printf("Length of the string = %d\n", length);
+
+    return 0;
+}
